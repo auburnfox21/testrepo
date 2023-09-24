757660dda44429564a414ee0c9b819e2ce3f8e46
auburnfox21 <116014146+auburnfox21@users.noreply.github.com>
2023-09-24 15:26:31 -0700

    Add files via upload
    Correct lab

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Author 
+# Faith Loy 
+# 
+# 
+# # Data Science Tools and Ecosystems 
+# 
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized. 
+# **Objectives:**
+# - R and R studio are used by data scientists to create visualizations and plot data.
+# - Git Hub and Git Lab are used by data scientists to share and collaborate on code.
+# - Jupyter Notebooks is an open source tool for storing, deploying, and writing programming languages. 
+# 
+
+# ### Some of the popular languages that Data Science use are:
+# 1. R
+# 2. Python
+# 3. Julia 
+# 
+# 
+# ### Some of the popular libriaires in Data Science include:
+# 1. Numpy
+# 2. TensorFlow
+# 3. Pandas
+
+# | Data Science Tools |
+# | ------------------ | 
+# | R Studio | 
+# |Apache Spark |
+# |Jupyter Notebook |  
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# In[ ]:
+
+
+*Objectives:*
+
